a0abde0529b4cd5dcf7352a8e7c5805f108029dc
liev <LuizEGouvea@outlook.com>
2026-03-04 17:53:13 -0300

    front end changes
    CSS adjustments for login and register pages;
    logic for register process.

--- Frontend/FinProFE/src/app/pages/register/register.component.ts
+++ Frontend/FinProFE/src/app/pages/register/register.component.ts
@@ -1,10 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  username = '';
+  password = '';
+  confirmPassword = '';
+  email = '';
+  error = '';
 
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {}
+
+  register() {
+    this.authService
+      .register(this.username, this.password, this.email)
+      .subscribe({
+        next: () => {
+          console.log('User created');
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          this.error = err.error?.message || 'Registration failed';
+        },
+      });
+  }
 }
--- Frontend/FinProFE/src/app/services/auth.service.ts
+++ Frontend/FinProFE/src/app/services/auth.service.ts
@@ -2,26 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private api = 'http://localhost:5172/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   login(username: string, password: string) {
     return this.http.post<any>(`${this.api}/login`, {
       name: username,
-      password: password
+      password: password,
     });
   }
 
-  saveToken(token: string){
+  saveToken(token: string) {
     localStorage.setItem('token', token);
   }
-  getToken(): string | null{
+  getToken(): string | null {
     return localStorage.getItem('token');
   }
-  logOut(){
+  register(name: string, password: string, email: string) {
+    return this.http.post<any>(`${this.api}/register`, {
+      name,
+      password,
+      email,
+    });
+  }
+  logOut() {
     localStorage.removeItem('token');
   }
 }
